419c0ff86db92bdf953984bed478ac591a0a6cc7
Christopher Hwang <77209332+SwagMasterDave@users.noreply.github.com>
2024-06-20 12:24:59 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,45 +7,44 @@ from sklearn.metrics import mean_squared_error, r2_score
 import altair as alt
 import time
 import zipfile
+from pyvis.network import Network
 
 # Page title
-st.set_page_config(page_title='ML Model Building', page_icon='🤖')
+st.set_page_config(page_title='ML Model Building', page_icon='🤖', layout='wide')
 st.title('🤖 ML Model Building')
 
 with st.expander('About this app'):
-  st.markdown('**What can this app do?**')
-  st.info('This app allow users to build a machine learning (ML) model in an end-to-end workflow. Particularly, this encompasses data upload, data pre-processing, ML model building and post-model analysis.')
+    st.markdown('**What can this app do?**')
+    st.info('This app allows users to build a machine learning (ML) model in an end-to-end workflow, including data upload, pre-processing, model building, and post-model analysis.')
 
-  st.markdown('**How to use the app?**')
-  st.warning('To engage with the app, go to the sidebar and 1. Select a data set and 2. Adjust the model parameters by adjusting the various slider widgets. As a result, this would initiate the ML model building process, display the model results as well as allowing users to download the generated models and accompanying data.')
+    st.markdown('**How to use the app?**')
+    st.warning('To engage with the app, go to the sidebar and: 1. Select a dataset and 2. Adjust the model parameters using the various slider widgets. This will initiate the ML model building process, display the model results, and allow users to download the generated models and data.')
 
-  st.markdown('**Under the hood**')
-  st.markdown('Data sets:')
-  st.code('''- Drug solubility data set
-  ''', language='markdown')
+    st.markdown('**Under the hood**')
+    st.markdown('Data sets:')
+    st.code('- Drug solubility data set', language='markdown')
 
-  st.markdown('Libraries used:')
-  st.code('''- Pandas for data wrangling
+    st.markdown('Libraries used:')
+    st.code('''- Pandas for data wrangling
 - Scikit-learn for building a machine learning model
 - Altair for chart creation
 - Streamlit for user interface
-  ''', language='markdown')
-
+- PyVis for network visualization
+    ''', language='markdown')
 
 # Sidebar for accepting input parameters
 with st.sidebar:
-    # Load data
     st.header('1.1. Input data')
 
     st.markdown('**1. Use custom data**')
     uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
     if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file, index_col=False)
+        df = pd.read_csv(uploaded_file)
 
-    # Download example data
     @st.cache_data
     def convert_df(input_df):
         return input_df.to_csv(index=False).encode('utf-8')
+
     example_csv = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv')
     csv = convert_df(example_csv)
     st.download_button(
@@ -55,7 +54,6 @@ with st.sidebar:
         mime='text/csv',
     )
 
-    # Select example data
     st.markdown('**1.2. Use example data**')
     example_data = st.toggle('Load example data')
     if example_data:
@@ -76,21 +74,35 @@ with st.sidebar:
         parameter_random_state = st.slider('Seed number (random_state)', 0, 1000, 42, 1)
         parameter_criterion = st.select_slider('Performance measure (criterion)', options=['squared_error', 'absolute_error', 'friedman_mse'])
         parameter_bootstrap = st.select_slider('Bootstrap samples when building trees (bootstrap)', options=[True, False])
-        parameter_oob_score = st.select_slider('Whether to use out-of-bag samples to estimate the R^2 on unseen data (oob_score)', options=[False, True])
+        parameter_oob_score = st.select_slider('Use out-of-bag samples to estimate R^2 on unseen data (oob_score)', options=[False, True])
 
     sleep_time = st.slider('Sleep time', 0, 3, 0)
 
-# Initiate the model building process
-if uploaded_file or example_data: 
-    with st.status("Running ...", expanded=True) as status:
+# Network visualization
+if 'df' in locals():
+    st.sidebar.header('Node Network')
+    network = Network(height='400px', width='100%', bgcolor='#222222', font_color='white')
 
+    # Create nodes and edges for the network (Example: simple linear network)
+    for i in range(len(df.columns)):
+        network.add_node(i, label=df.columns[i])
+
+    for i in range(len(df.columns)-1):
+        network.add_edge(i, i+1)
+
+    # Save and display the network
+    network.save_graph('network.html')
+    st.sidebar.components.v1.html(open('network.html', 'r').read(), height=400)
+
+    # Main process
+    with st.status("Running ...", expanded=True) as status:
         st.write("Loading data ...")
         time.sleep(sleep_time)
 
         st.write("Preparing data ...")
         time.sleep(sleep_time)
-        X = df.iloc[:,:-1]
-        y = df.iloc[:,-1]
+        X = df.iloc[:, :-1]
+        y = df.iloc[:, -1]
 
         st.write("Splitting data ...")
         time.sleep(sleep_time)
@@ -99,13 +111,11 @@ if uploaded_file or example_data:
         st.write("Model training ...")
         time.sleep(sleep_time)
 
-        if parameter_max_features == 'all':
-            parameter_max_features = None
-            parameter_max_features_metric = X.shape[1]
+        max_features = parameter_max_features if parameter_max_features != 'all' else None
 
         rf = RandomForestRegressor(
                 n_estimators=parameter_n_estimators,
-                max_features=parameter_max_features,
+                max_features=max_features,
                 min_samples_split=parameter_min_samples_split,
                 min_samples_leaf=parameter_min_samples_leaf,
                 random_state=parameter_random_state,
@@ -128,26 +138,17 @@ if uploaded_file or example_data:
 
         st.write("Displaying performance metrics ...")
         time.sleep(sleep_time)
-        parameter_criterion_string = ' '.join([x.capitalize() for x in parameter_criterion.split('_')])
-        #if 'Mse' in parameter_criterion_string:
-        #    parameter_criterion_string = parameter_criterion_string.replace('Mse', 'MSE')
-        rf_results = pd.DataFrame(['Random forest', train_mse, train_r2, test_mse, test_r2]).transpose()
-        rf_results.columns = ['Method', f'Training {parameter_criterion_string}', 'Training R2', f'Test {parameter_criterion_string}', 'Test R2']
-        # Convert objects to numerics
-        for col in rf_results.columns:
-            rf_results[col] = pd.to_numeric(rf_results[col], errors='ignore')
-        # Round to 3 digits
-        rf_results = rf_results.round(3)
+        rf_results = pd.DataFrame([['Random forest', train_mse, train_r2, test_mse, test_r2]], columns=['Method', f'Training {parameter_criterion}', 'Training R2', f'Test {parameter_criterion}', 'Test R2']).round(3)
 
     status.update(label="Status", state="complete", expanded=False)
 
     # Display data info
-    st.header('Input data', divider='rainbow')
+    st.header('Input data')
     col = st.columns(4)
-    col[0].metric(label="No. of samples", value=X.shape[0], delta="")
-    col[1].metric(label="No. of X variables", value=X.shape[1], delta="")
-    col[2].metric(label="No. of Training samples", value=X_train.shape[0], delta="")
-    col[3].metric(label="No. of Test samples", value=X_test.shape[0], delta="")
+    col[0].metric(label="No. of samples", value=X.shape[0])
+    col[1].metric(label="No. of X variables", value=X.shape[1])
+    col[2].metric(label="No. of Training samples", value=X_train.shape[0])
+    col[3].metric(label="No. of Test samples", value=X_test.shape[0])
 
     with st.expander('Initial dataset', expanded=True):
         st.dataframe(df, height=210, use_container_width=True)
@@ -155,18 +156,18 @@ if uploaded_file or example_data:
         train_col = st.columns((3,1))
         with train_col[0]:
             st.markdown('**X**')
-            st.dataframe(X_train, height=210, hide_index=True, use_container_width=True)
+            st.dataframe(X_train, height=210, use_container_width=True)
         with train_col[1]:
             st.markdown('**y**')
-            st.dataframe(y_train, height=210, hide_index=True, use_container_width=True)
+            st.dataframe(y_train, height=210, use_container_width=True)
     with st.expander('Test split', expanded=False):
         test_col = st.columns((3,1))
         with test_col[0]:
             st.markdown('**X**')
-            st.dataframe(X_test, height=210, hide_index=True, use_container_width=True)
+            st.dataframe(X_test, height=210, use_container_width=True)
         with test_col[1]:
             st.markdown('**y**')
-            st.dataframe(y_test, height=210, hide_index=True, use_container_width=True)
+            st.dataframe(y_test, height=210, use_container_width=True)
 
     # Zip dataset files
     df.to_csv('dataset.csv', index=False)
@@ -189,61 +190,19 @@ if uploaded_file or example_data:
                 )
 
     # Display model parameters
-    st.header('Model parameters', divider='rainbow')
+    st.header('Model parameters')
     parameters_col = st.columns(3)
-    parameters_col[0].metric(label="Data split ratio (% for Training Set)", value=parameter_split_size, delta="")
-    parameters_col[1].metric(label="Number of estimators (n_estimators)", value=parameter_n_estimators, delta="")
-    parameters_col[2].metric(label="Max features (max_features)", value=parameter_max_features_metric, delta="")
+    parameters_col[0].metric(label="Data split ratio (% for Training Set)", value=parameter_split_size)
+    parameters_col[1].metric(label="Number of estimators (n_estimators)", value=parameter_n_estimators)
+    parameters_col[2].metric(label="Max features (max_features)", value=max_features if max_features else X.shape[1])
 
     # Display feature importance plot
     importances = rf.feature_importances_
     feature_names = list(X.columns)
     forest_importances = pd.Series(importances, index=feature_names)
-    df_importance = forest_importances.reset_index().rename(columns={'index': 'feature', 0: 'value'})
+    df_importance = forest_importances
 
-    bars = alt.Chart(df_importance).mark_bar(size=40).encode(
-             x='value:Q',
-             y=alt.Y('feature:N', sort='-x')
-           ).properties(height=250)
-
-    performance_col = st.columns((2, 0.2, 3))
-    with performance_col[0]:
-        st.header('Model performance', divider='rainbow')
-        st.dataframe(rf_results.T.reset_index().rename(columns={'index': 'Parameter', 0: 'Value'}))
-    with performance_col[2]:
-        st.header('Feature importance', divider='rainbow')
-        st.altair_chart(bars, theme='streamlit', use_container_width=True)
-
-    # Prediction results
-    st.header('Prediction results', divider='rainbow')
-    s_y_train = pd.Series(y_train, name='actual').reset_index(drop=True)
-    s_y_train_pred = pd.Series(y_train_pred, name='predicted').reset_index(drop=True)
-    df_train = pd.DataFrame(data=[s_y_train, s_y_train_pred], index=None).T
-    df_train['class'] = 'train'
-
-    s_y_test = pd.Series(y_test, name='actual').reset_index(drop=True)
-    s_y_test_pred = pd.Series(y_test_pred, name='predicted').reset_index(drop=True)
-    df_test = pd.DataFrame(data=[s_y_test, s_y_test_pred], index=None).T
-    df_test['class'] = 'test'
-
-    df_prediction = pd.concat([df_train, df_test], axis=0)
-
-    prediction_col = st.columns((2, 0.2, 3))
-
-    # Display dataframe
-    with prediction_col[0]:
-        st.dataframe(df_prediction, height=320, use_container_width=True)
-
-    # Display scatter plot of actual vs predicted values
-    with prediction_col[2]:
-        scatter = alt.Chart(df_prediction).mark_circle(size=60).encode(
-                        x='actual',
-                        y='predicted',
-                        color='class'
-                  )
-        st.altair_chart(scatter, theme='streamlit', use_container_width=True)
-
-
-# Ask for CSV upload if none is detected
+    st.header('Feature Importance')
+    st.bar_chart(df_importance.sort_values(ascending=False))
 else:
-    st.warning('👈 Upload a CSV file or click *"Load example data"* to get started!')
+    st.warning('Please upload a dataset or load example data to proceed.')
